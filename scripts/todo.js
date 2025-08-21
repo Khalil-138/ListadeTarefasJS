1c622878e9f0aa3333d3324a9a107861c46bc992
Khalil <khalilsantana92@gmail.com>
2025-08-20 21:29:16 -0300

    feat-todo

---
+++ scripts/todo.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const todoInput = document.getElementById('todo-input');
+    const addButton = document.getElementById('add-button');
+    const todoList = document.getElementById('todo-list');
+
+    addButton.addEventListener('click', addTodo)
+
+    function addTodo() {
+        const texto = todoInput.value.trim()
+        //pegar o que ta escrito no input       
+        // verificar se tem algo
+        if (texto !== '') {
+            //add lista
+            const li = document.createElement('li');
+            li.textContent = texto
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = "Deletar";
+            deleteButton.className = "delete-button";
+            deleteButton.addEventListener('click', () => {
+                //remover o li do todo lista
+                todoList.removeChild(li);
+            })
+            li.appendChild(deleteButton);
+            todoList.appendChild(li);
+            todoInput.value = '';
+            todoInput.focus();
+        }
+    }
+
+
+})
